2249d87184e50916da20cf01465d439dd8bd8dac
enzo <renzo.surell@mseuf.edu.ph>
2026-08-10 00:13:14 +0800

    Props vs attributes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{$title}} </title>
+
+    <style>
+        .card {
+            background: #e3e3e3;
+            padding: 1rem;
+            text-align: center;
+            max-width: 400px;
+            margin: auto;
+        }
+    </style>
 </head>
 <body>
     
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,7 @@
 <x-layout title="Contact Us">
     <h1>Contact Us Page.</h1>
+    
+    <x-card class="card">
+	    <p>Place holder for the Contact Us Page...</p>
+    </x-card>
 </x-layout>
