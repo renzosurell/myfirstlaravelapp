7980628e88b8cfa84a2d8800606a8ac84a67b120
enzo <renzo.surell@mseuf.edu.ph>
2026-08-10 17:54:50 +0800

    Blade directives

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,28 @@
 <x-layout title="Home Page">
     <h1>Welcome Home!</h1>
-    <p>
-        {{$greetings}}, {{$person}}!
-    </p>
+
+    {{--
+    
+    @dump($tasks)
+
+
+    @if (count($tasks))
+        <p>We have some tasks. {{ count($tasks) }} tasks, in fact!</p>
+    @else
+        <p> We don't have task yet.</p>
+    @endif
+
+
+    @foreach ($tasks as $task)
+        <li> {{ $task }} </li>
+    @endforeach
+
+    --}}
+
+    @forelse ($tasks as $task)
+        <li> {{ $task }} </li>
+    @empty
+        <p> We don't have task yet.</p>
+    @endforelse
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::view('/','welcome');
-
 Route::get('/', function () {
     return view('welcome', [
-    'greetings'=>'Good evening',
-    'person'=>'enzo',
+    'tasks' => [
+        'Wash laundry',
+        'Finish Modules',
+        'Clean room',
+    ],
     ]);
 });
-
-*/
-
-Route::get('/', function () {
-    return view('welcome', [
-    'greetings'=>'Good evening',
-    'person'=>request('person', 'Jane Doe'),
-    ]);
-});
-
-// add /?person='enzo' on the URL
 
 Route::view('/about','about');
 Route::view('/contact','contact');
